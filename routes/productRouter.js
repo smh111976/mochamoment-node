22dc1f00669c58bb83f7aeeb208661a0b7defd42
Scott Henning <scott@scotthenning.info>
2022-02-25 11:00:05 -0600

    added routers

---
+++ routes/productRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const productRouter = express.Router();
+
+productRouter
+  .route('/')
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+  })
+  .get((req, res) => {
+    res.end('Will send all the products to you');
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the product: ${req.body.name} with description: ${req.body.description} and price: ${req.body.price}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /products');
+  })
+  .delete((req, res) => {
+    res.end('Deleting all products');
+  });
+module.exports = productRouter;
